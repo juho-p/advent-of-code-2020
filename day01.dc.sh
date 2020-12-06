17a3a6a323905af2f549fe16ae69adc638728b51
Juho Peltonen <juhopel@gmail.com>
2020-12-06 20:32:23 +0200

    day 1 part 2 with silly dc

--- day01.dc.sh
+++ day01.dc.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
+{ cat day01.txt
+echo '
+[dlx+2020=clzz=dlz1+szzRlax]sa[lx*pcq]sc[1szsxlaxq]sdlax
+'
+} | dc
+
 {
-    cat day01.txt
-    echo '[dlx+2020=clzz=dlz1+szzRlax]sa[lx*pcq]sc[1szsxlaxq]sdlax'
+cat day01.txt
+echo '
+zsl[li:xli1+dsill!=a]salax
+[d;x0lbx-st1+dll!=a]sa
+[rdstrd;xlt+]sf
+[lfx0lcx-st1+dll!=b]sb
+[lfx2020=d1+dll!=c]sc
+[rst3Rst;xr;x*r;x*pc4Q]sd
+0lax'
 } | dc
